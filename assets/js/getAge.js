fa2d486499371dfdbf450f4e85d21b77ebf0c9d1
Clement Carpot <clement.carpot@aquasys.fr>
2023-02-24 15:59:41 +0100

    update Portfolio

--- assets/js/getAge.js
+++ assets/js/getAge.js
@@ -1,8 +1,5 @@
-function calculate_age(dob) { 
-    var diff_ms = Date.now() - dob.getTime();
-    var age_dt = new Date(diff_ms); 
-  
+const calculate_age = (dob) => {
+    const diff_ms = Date.now() - dob.getTime();
+    const age_dt = new Date(diff_ms);
     return Math.abs(age_dt.getUTCFullYear() - 1970);
 }
-
-console.log(calculate_age(new Date(1998, 1, 24)));
